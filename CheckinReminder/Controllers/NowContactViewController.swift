a8794b3575f808df1de881c450e742254212053d
Aaron Tong <ytong3@utk.edu>
2019-08-13 21:07:28 -0400

    initial code

---
+++ CheckinReminder/Controllers/NowContactViewController.swift
@@ -0,0 +1,23 @@
+//
+//  NowContactViewController.swift
+//  CheckinReminder
+//
+//  Created by Yue Tong on 8/13/19.
+//  Copyright © 2019 Biang Studio. All rights reserved.
+//
+
+import UIKit
+
+class NowContactViewController: UITableViewController{
+    //MARK: - Properties
+    var contacts: [Contact]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+//MARK: - Data Source
+extension NowContactViewController{
+    
+}
---
+++ CheckinReminder/Models/Contact.swift
@@ -0,0 +1,12 @@
+//
+//  Contact.swift
+//  CheckinReminder
+//
+//  Created by Yue Tong on 8/13/19.
+//  Copyright © 2019 Biang Studio. All rights reserved.
+//
+
+import Foundation
+
+class Contact {
+}
